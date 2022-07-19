678ccc13bbc5ef35e723da865723819994887500
Henok <henok_206@yahoo.com>
2022-07-19 16:54:36 +0300

    pro commit

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-let iconInput = document.querySelector(".iconInpu");
 let titleInput = document.querySelector(".titleInp");
-let table = document.querySelector(".table");
+let iconInput = document.querySelector(".iconInpu");
+let mainContainer = document.querySelector(".mainContainer");
 let iconElm = document.querySelectorAll(".icon");
 let textEl = document.querySelectorAll(".text");
+//creating new Html Element
+//create div containerElement
+let newcontainer = document.createElement("div");
+newDiv.classList.add("container");
+let newDiv = document.createElement("div");
+newDiv.classList.add("box");
+// let helm = document.createElement("h3");
+// helm.classList("icon");
+// helm.innerHTML = "<h3>&#9730</h3>";
+// newDiv.appendChild(helm);
+// let teEl = document.createElement("p");
+// teEl.classList.add("text");
+// teEl.innerText = "rain";
+// newDiv.appendChild(teEl);
+newcontainer.insertAdjacentHTML("afterbegin", newDiv);
+mainContainer.insertAdjacentHTML("beforeend", newcontainer);
+
 const addButton = document.querySelector(".addBtn");
 
 addButton.addEventListener("click", function () {
@@ -12,6 +29,7 @@ addButton.addEventListener("click", function () {
         iconElm[i].style.backgroundColor = "blue";
         textEl[i].style.color = "green";
     }
+
     // newIcon.style.color = "white";
     // newIcon.style.width = "90px";
     // newIcon.style.borderRadius = "50%";
@@ -19,12 +37,10 @@ addButton.addEventListener("click", function () {
     // newIcon.style.paddingTop = "30px";
     // newIcon.style.display = "inline-block";
 
-    let template = `<tr>
-          <td>${newIcon}
-           ${newTitle}
-          </td>
-         </tr>`;
-    table.innerHTML += template;
+    // let template = `<div class="box'>
+    //         <div class="box">${newIcon}${newTitle}</div>
+    // </div>`;
+    // boxContainer.innerHTML += template;
 });
 /* width: 90px;
     height: 60px;
